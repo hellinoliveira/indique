f2879efa5c826ed30cd87472f9a4efd4fcd90f7a
Hellison Oliveira <hellison.oliveira@gmail.com>
2016-04-27 14:38:44 -0300

    esboco do rodape

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,159 @@
+<style>
+    .footer-link {
+        margin-top: 2%;
+        opacity: 0.5;
+        -webkit-transition: all 250ms ease;
+        transition: all 250ms ease;
+        color: #6e6e6e;
+        text-decoration: none;
+    }
+    .footer-link:hover {
+        opacity: 1;
+        color: white;
+    }
+    .footer-link.nav {
+        height: 70px;
+        margin-top: 25px;
+        margin-bottom: 25px;
+    }
+    .footer-nav-title {
+        -webkit-transform: translate(0px, 0px);
+        -ms-transform: translate(0px, 0px);
+        transform: translate(0px, 0px);
+    }
+    .footer-section {
+        position: static;
+    }
+    .hotnews-title {
+        font-size: 12px;
+    }
+    .footer-title {
+        margin-bottom: 5px;
+    }
+    .wrapper.footer {
+        padding-top: 25px;
+        padding-bottom: 20px;
+    }
+    .wrapper.half {
+        padding-top: 30px;
+    }
+    .wrapper.page-content {
+        padding-top: 50px;
+    }
+    .wrapper.form {
+        position: static;
+        width: 100%;
+        height: 450px;
+        padding-top: 60px;
+        padding-bottom: 60px;
+    }
+    .footer-link {
+        margin-top: 15px;
+    }
+    .footer-link.nav {
+        display: block;
+        width: 100%;
+        margin-top: 0px;
+        margin-right: auto;
+        margin-left: auto;
+        padding-top: 15px;
+        float: left;
+    }
+    .footer-link:hover .menu-icon-block  {
+        background-color: #aec915;
+    }
+    .footer-link:hover .button.back {
+        color: #aec915;
+    }
+    .menu-icon-block._1 {
+        left: 0px;
+        top: 0px;
+    }
+    .footer-link:hover .menu-icon-block._1 {
+        left: 5px;
+        top: 5px;
+    }
+    .menu-icon-block._2 {
+        left: 10px;
+        top: 0px;
+        z-index: 10;
+    }
+    .footer-link:hover .menu-icon-block._2 {
+        left: 0px;
+    }
+    .menu-icon-block._3 {
+        left: 20px;
+        top: 0px;
+    }
+    .footer-link:hover .menu-icon-block._3 {
+        left: 15px;
+        top: 5px;
+    }
+    .menu-icon-block._4 {
+        left: 0px;
+        top: 10px;
+    }
+    .footer-link:hover .menu-icon-block._4 {
+        top: 20px;
+    }
+    .menu-icon-block._5 {
+        left: 10px;
+        top: 10px;
+    }
+    .menu-icon-block._6 {
+        left: 20px;
+        top: 10px;
+    }
+    .footer-link:hover .menu-icon-block._6 {
+        top: 0px;
+    }
+    .menu-icon-block._7 {
+        left: 0px;
+        top: 20px;
+    }
+    .footer-link:hover .menu-icon-block._7 {
+        left: 5px;
+        top: 15px;
+    }
+    .menu-icon-block._8 {
+        left: 10px;
+        top: 20px;
+    }
+    .footer-link:hover .menu-icon-block._8 {
+        left: 20px;
+    }
+    .menu-icon-block._9 {
+        left: 20px;
+        top: 20px;
+    }
+    .footer-link:hover .menu-icon-block._9 {
+        left: 15px;
+        top: 15px;
+    }
+</style>
+<div class="wrapper footer">
+    <div class="social-block"><h4 class="footer-title">Rc comunicação</h4>
+        <ul class="w-list-unstyled">
+            <li class="list-item social"><a class="w-inline-block social-icon facebook" target="_blank"
+                                            href="https://www.facebook.com/rccomunicacao"></a></li>
+            <li class="list-item social"><a class="w-inline-block social-icon twitter" target="_blank"
+                                            href="https://twitter.com/rccombr"></a></li>
+            <li class="list-item social"><a class="w-inline-block social-icon youtube" target="_blank"
+                                            href="https://www.youtube.com/user/filmesrc"></a></li>
+            <li class="list-item social"><a class="w-inline-block social-icon pinteres" target="_blank"
+                                            href="https://www.pinterest.com/rccomunicacao/"></a></li>
+            <li class="list-item social"><a class="w-inline-block social-icon gplus" target="_blank"
+                                            href="https://plus.google.com/109328716795066219712"></a></li>
+        </ul>
+    </div>
+    <div class="phone-block"><h4 class="footer-title">Telefone</h4> <a class="w-clearfix w-inline-block footer-link"
+                                                                       href="tel:+553130365555">
+            <div class="icon phone-icon"></div>
+            <p class="phone"><span class="small-phone">+ 55 </span>31 3036-5555</p></a></div>
+    <address class="address-block"><h4 class="footer-title">endereço</h4> <a
+                class="w-clearfix w-inline-block footer-link"
+                href="https://www.google.com.br/maps/dir/''/rc+comunica%C3%A7%C3%A3o/@-19.9307235,-43.9451382,15z/data=!4m8!4m7!1m0!1m5!1m1!1s0xa699df1d2ed15f:0xc6aae1c86794109!2m2!1d-43.941404!2d-19.931574"
+                target="_blank">
+            <div class="icon location"></div>
+            <p class="address">Rua Alvarenga Peixoto, 295, 2º andar<br>Lourdes – Belo Horizonte / MG</p></a></address>
+</div>
